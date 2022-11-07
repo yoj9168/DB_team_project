597f0c243c252fd0a115ba92955c0b67b1979f6f
yoj9168@gmail.com <yoj9168@gmail.com>
2022-11-07 13:00:21 +0900

    merge완료

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -5,6 +5,8 @@ function getCheckboxValue()  {
     const query = 'input[name="search"]:checked';
     const selectedEls =
         document.querySelectorAll(query);
+    const rangeValue = document.getElementById('searchRange').value
+    const inputValue = document.querySelector('input[name="customInputField"]').value
 
     // 선택된 목록에서 value 찾기
     let result = [];
@@ -19,17 +21,40 @@ function getCheckboxValue()  {
         }
     });
 
-    console.log(result);
+    postAjax(result, rangeValue, inputValue);
+}
+
+function postAjax(_attribuite, _selectRange, _search)
+{
+    // Type은 List<String>, String, String 입니다.
+    let testObj = {attribute : _attribuite, selectRange : _selectRange, search : _search}
+
+    console.log(testObj);
     $.ajax({
         type: 'POST',
         url:'/employee',
-        data: JSON.stringify(result),
+        data: JSON.stringify(testObj),
         dataType:'json',
-        contentType:'application/json; charset=utf-8',
+        contentType:'application/json; charset=utf-8'
     }).done(function(rs) {
         alert('POST 성공');
-        console.log(JSON.stringify(rs));
+        console.log(replaceNullorZero(rs));
     }).fail(function (error) {
         alert(JSON.stringify(error));
     });
+}
+
+function replaceNullorZero(_objs)
+{
+    console.log(_objs);
+    const keys = Object.keys(_objs[0]);
+    _objs.forEach((v) => {
+        for(let i = 0; i < keys.length ; i++)
+        {
+            if(v[keys[i]] == null || v[keys[i]] == 0)
+                v[keys[i]] = ""
+        }
+    })
+
+    return _objs;
 }
